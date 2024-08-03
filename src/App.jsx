29e72863d48e576c3b51afe3e847fb74ce9a2bdb
Rahulkapta <110374873+Rahulkapta@users.noreply.github.com>
2024-08-03 15:28:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+// import { useState } from 'react'
+
+import Header from "./components/Header/Header"
+
+// import './App.css'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='bg-gray-700 text-center p-4'>React Routers</h1>
+      <Header/>
+    </>
+  )
+}
+
+export default App
